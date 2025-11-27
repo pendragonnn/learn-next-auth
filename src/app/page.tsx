c72c8fc8e7e8e914296a1e95517cb8ce24b9b55f
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-11-27 08:57:37 +0700

    feat: creating protected routes with middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,21 @@
 import { auth } from "@/auth";
 
 import { SignInButton } from "./components/sign-in-button";
+import Image from "next/image";
 
 export default async function Home() {
   const session = await auth()
-  console.log({ session})
+  // console.log(session)
+  if(session?.user) {
+    return (
+      <div className="flex items-center justify-center flex-col min-h-screen">
+        <h1 className="text-2xl">Hello there, {session.user.name}</h1>
+        {session.user.image && (
+          <Image className="rounded-full my-4" src={session.user.image} alt="user image" width={100} height={100}/>
+        )}
+      </div>
+    )
+  }
   return (
     <div className="flex items-center justify-center flex-col min-h-screen">
       {" "}
